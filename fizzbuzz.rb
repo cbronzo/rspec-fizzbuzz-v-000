1d3c3538fed40e17a1fe47ba851409289d4c9eb0
Catherine Bronzo <cbronzo@mac.com>
2018-05-31 03:40:53 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -3,7 +3,9 @@ def fizzbuzz(int)
       "Fizz"
     elsif int % 5 == 0
       "Buzz"
-    else int % 3 && 5 == 0
+    elseif int % 3 && 5 == 0
       "FizzBuzz"
+    else int !==0
+      "nil"
   end
 end
